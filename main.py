b699baf5adcfd6a77edb5d8b865993b005d38f16
Zhou <176358@mcpsmd.org>
2025-10-16 14:26:27 -0400

    hiii

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# hi anna
+from enum import Enum
+
+class messageType(Enum):
+    000 = Connect
+    001 = Set Tempo
+    010 = Start
+    011 = Stop
+    100 = Alert
+
+class conductorID(Enum):
+    00 = 0
+    01 = 1    
+    10 = 2
+    11 = 3
